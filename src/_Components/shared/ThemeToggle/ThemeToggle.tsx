13264af93377b62b69b026f0735ab7006c1c7ada
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2025-12-04 17:07:51 +0200

    disable system prefer theme due to hydration error

--- src/_Components/shared/ThemeToggle/ThemeToggle.tsx
+++ src/_Components/shared/ThemeToggle/ThemeToggle.tsx
@@ -1,13 +1,19 @@
 'use client'
-import { Button } from "@/_Components/ui/button"
-import { useTheme } from "next-themes"
-import { Icons } from '@/_Components/icons/icons';
+import { Button } from "@/_Components/ui/button";
+import { useMounted } from "@/app/_hooks/useMounted";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export default function ThemeToggle() {
     const { theme, setTheme } = useTheme()
+    const mounted = useMounted();
+    if (!mounted) {
+        return null; 
+    }
+
     return (
         <Button className="bg-primary text-gray-600  hover:bg-gray-200 hover:text-black dark:hover:bg-white dark:hover:text-black dark:text-yellow-400 dark:bg-black" variant={'ghost'} size='icon' onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-            {theme === 'dark' ? <Icons.sun /> : <Icons.moon />}
+            {theme === 'dark' ? <Sun /> : <Moon />}
             <span className="sr-only">Theme Toggle</span>
         </Button>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
+          enableSystem ={false}
+          
         >
 
           <MainProvider>
